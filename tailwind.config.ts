e61ce68d1057759b60bea2d3feb28b7dcc916e42
Alexei Mamani <alexei@sfleet.mx>
2025-11-19 00:32:40 -0500

    chore: Prepare for Tailwind v4 upgrade

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
-/** @type {import('tailwindcss').Config} */
+import type { Config } from 'tailwindcss'
+
 export default {
   content: [
     "./index.html",
@@ -50,4 +51,4 @@ export default {
     },
   },
   plugins: [],
-}
+} satisfies Config
